08faab217a106059a20b96cdbe0804b2a0154f4e
Samuel Diaz Bidot <samuel.diaz9@upr.edu>
2021-05-28 12:43:32 -0400

    finished TS chackra-ui setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+import { ChakraProvider, theme } from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+  </ChakraProvider>
+)
